caef922b2ad5a49aab766ca703479a86391e93ac
MiguelHerazo7 <herazomigue56@gmail.com>
2025-04-29 18:45:36 -0500

    Desarrollo preliminar de la barra de navegacion

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  menuActive = false;
 
+  toggleMenu(): void {
+    this.menuActive = !this.menuActive;
+  }
+
+  login(): void {
+    console.log('Iniciar sesión');
+  }
 }
